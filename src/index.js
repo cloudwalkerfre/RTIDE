7cdd25cf59b5f1fe7e3c8846e2ab5f5dd15cf6a3
Desmond <cloudwalkerfree@gmail.com>
2019-12-27 10:49:05 +0800

    exclude rsuite css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import 'rsuite/dist/styles/rsuite-default.css'
+// import 'rsuite/dist/styles/rsuite-default.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
